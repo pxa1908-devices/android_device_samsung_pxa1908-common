43d94660383151c2dfe871ef610b0fa4ebfc4d16
notnoelchannel2 <notnoelchannel1@protonmail.com>
2026-07-28 15:01:41 +0200

    update for Oreo

--- init/Android.mk
+++ init/Android.mk
@@ -3,12 +3,25 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := system/core/init
-LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
+
+# Added base/include and core/include for Oreo libbase compatibility
+LOCAL_C_INCLUDES := \
+    system/core/init \
+    system/core/base/include \
+    system/core/include
+
+# Added -Wno-error to prevent legacy warnings from stopping the build
+LOCAL_CFLAGS := -Wall -Wno-error -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
+
 LOCAL_SRC_FILES := init_pxa1908.cpp
+
 ifneq ($(TARGET_LIBINIT_PXA1908_DEFINES_FILE),)
   LOCAL_SRC_FILES += ../../../../$(TARGET_LIBINIT_PXA1908_DEFINES_FILE)
 endif
+
 LOCAL_MODULE := libinit_pxa1908
+
+# Link libbase so android-base dependencies are resolved
+LOCAL_STATIC_LIBRARIES += libbase
 
 include $(BUILD_STATIC_LIBRARY)
