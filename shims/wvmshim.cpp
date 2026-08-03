68c8d8fefbc35a6c9a3fa28e49ed8140efd31b5a
notnoelchannel2 <notnoelchannel1@protonmail.com>
2026-08-03 20:33:22 +0200

    Delete shims/wvmshim.cpp

--- shims/wvmshim.cpp
+++
@@ -1,6 +0,0 @@
-/* MediaBufferGroup::MediaBufferGroup() wrapper */
-extern "C" void _ZN7android16MediaBufferGroupC1Ej(unsigned int);
-
-extern "C" void _ZN7android16MediaBufferGroupC1Ev() {
-    _ZN7android16MediaBufferGroupC1Ej(0);
-}
